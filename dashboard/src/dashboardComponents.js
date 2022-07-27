e0317b492b3d58a71f21fed43fd3489844259f12
gideona1 <75781877+gideona1@users.noreply.github.com>
2022-07-27 00:18:36 -0400

    Home and Manage User Progress

---
+++ dashboard/src/dashboardComponents.js
@@ -0,0 +1,147 @@
+customElements.define(
+  "cg-ds-home",
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      this.innerHTML = `
+      <div class="cg-ds-home">
+          <section>
+            <div class="cg-max-container" style="margin-top: 40px">
+              <ul class="breadcrumb">
+                <li><a href="#" class="current">Dashboard</a></li>
+              </ul>
+              <h1 style="font-size: 2rem">Home</h1>
+              <p class="cg-header-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis aut quisquam aliquid quidem expedita placeat magnam facere sit minima corrupti
+                et nemo, debitis sequi velit libero nostrum fugit ipsam! Sapiente.
+              </p>
+              <div class="cg-ds-info-container hidden" id="cg-user-not-verified">
+                <h2>Verification</h2>
+                <p>Your account has not been verified yet. Access to features are limited.</p>
+              </div>
+            </div>
+          </section>
+
+          <section class="secondary">
+            <div class="cg-max-container">
+              <h2>Resources</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quo!</p>
+              <div class="cg-ds-home-showcase">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+
+            <div class="cg-max-container">
+              <h2>Others</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quo!</p>
+              <div class="cg-ds-home-showcase">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </section>
+
+          <section class="accent-d">
+            <div class="cg-max-container">
+              <h2>Information</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi rem veniam similique. Ipsa, ex numquam expedita ut possimus iste odit
+                consectetur ea, odio sunt pariatur?
+              </p>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa deserunt quia earum! Officia ducimus doloremque aliquid blanditiis nostrum
+                suscipit aperiam rem explicabo laborum est minus magnam aspernatur placeat, quod itaque.
+              </p>
+            </div>
+          </section>
+        </div>`;
+    }
+  }
+);
+
+customElements.define(
+  "cg-ds-manage-user",
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      this.innerHTML = `
+      <div class="cg-ds-manage-user">
+        <section>
+          <div class="cg-max-container" style="margin-top: 40px">
+            <ul class="breadcrumb">
+              <li><a href="#">Dashboard</a></li>
+              <li><a href="#" class="current">Manage Users</a></li>
+            </ul>
+            <h1 style="font-size: 2rem">Manage Users</h1>
+            <p class="cg-header-description">View, verify, promote, and remove users.</p>
+          </div>
+        </section>
+
+        <section class="secondary hidden" id="cg-ds-no-access">
+          <div class="cg-max-container">
+            <h2>No Access</h2>
+            <p>Your account does not have privellegs to modify user accounts</p>
+            <button class="cg-link-btn light" onclick="getAllUsers()">Try again</button>
+          </div>
+        </section>
+
+        <section class="secondary" id="cg-ds-user-manage">
+          <div class="cg-max-container">
+            <button id="cg-ds-man-refresh" class="cg-link-btn light" onclick="getAllUsers()">Refresh <i class="fa-solid fa-arrows-rotate"></i></button>
+            <h2>Users</h2>
+            <div id="cg-sd-man-userDisplay"></div>
+          </div>
+        </section>
+      </div>`;
+    }
+  }
+);
+
+customElements.define(
+  "cg-ds-man-user-display",
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      const name = this.getAttribute("name");
+      const role = parseInt(this.getAttribute("role"));
+      const verified = this.getAttribute("verified");
+      const email = this.getAttribute("email");
+      const id = this.getAttribute("id");
+
+      const displayRole = role === 0 ? "Member" : role === 1 ? "Administrator" : role === 2 ? "Master" : "Unknown";
+      const displayVerified = verified === "true" ? "verified" : "not verified";
+
+      this.innerHTML = `
+      <div class="cg-ds-man-user-display">
+        <div class="user-information">
+          <div class="user-name">${name}</div>
+          <div class="user-role">${displayRole}, ${displayVerified}</div>
+          <div class="user-email">${email}</div>
+        </div>
+
+        <div class="user-action">
+          <button class="cg-link-btn light" style="margin-right: 5px">View</button>
+          <button class="cg-link-btn light" disabled="${verified === "true" ? "true" : "false"}">${verified === "true" ? "Verified" : "Verify"}</button>
+        </div>
+      </div>`;
+    }
+  }
+);
--- dashboard/src/dashboardScript.js
+++ dashboard/src/dashboardScript.js
@@ -11,6 +11,10 @@ const getUserData = () => {
     }
   `;
 
+  setInterval(() => {
+    document.getElementById("cg-ds-main-loading-ind").classList.add("loading");
+  }, 500);
+
   fetchServer(query, {}, (data, error) => {
     if (error) {
       console.log(error);
@@ -31,12 +35,16 @@ const logoutUser = () => {
     }
   `;
 
+  document.getElementById("cg-ds-logout-ind").classList.add("loading");
+
   fetchServer(query, {}, (data, error) => {
     if (error) {
       console.error("ERROR: " + error.message);
     } else {
       window.location.href = "../login";
     }
+
+    document.getElementById("cg-ds-logout-ind").classList.remove("loading");
   });
 };
 
@@ -47,12 +55,110 @@ const initialize = (userData) => {
 
   const name = userData["name"];
   const role = userData["role"];
+  const verified = userData["verified"];
 
-  document.querySelector(".cg-dashboard-lp-user > .name").textContent = name;
-  document.querySelector(".cg-dashboard-lp-user > .role").textContent = role === 0 ? "Member" : role === 1 ? "Admin" : role === 2 ? "Master" : "Unknown";
+  document.querySelector(".cg-dashboard-lp-user > .name > #cg-ds-display-name").textContent = name;
+  document.querySelector(".cg-dashboard-lp-user > .role").textContent =
+    role === 0 ? "Member" : role === 1 ? "Administrator" : role === 2 ? "Master" : "Unknown";
   console.log(userData);
 
-  document.querySelector("body").className = "";
+  // Verified & verified badge
+  if (!verified) document.getElementById("cg-user-not-verified").classList.remove("hidden");
+
+  const badge = document.createElement("i");
+  badge.className = verified ? "fa-solid fa-circle-check" : "fa-solid fa-circle-minus";
+  document.getElementById("cg-ds-display-verified").appendChild(badge);
+
+  document.querySelector(".cg-ds-loading-overlay").classList.add("hidden");
+  document.querySelector(".cg-fetching").classList.remove("cg-fetching");
 };
 
 getUserData();
+
+// Create Manage User Page
+const getAllUsers = () => {
+  const query = `
+    query GetAllUsers {
+      getAllUsers {
+        name
+        email
+        role
+        verified
+        id
+      }
+    }
+  `;
+
+  document.getElementById("cg-ds-man-refresh").classList.add("loading");
+
+  document.getElementById("cg-ds-no-access").classList.add("hidden");
+  document.getElementById("cg-ds-user-manage").classList.remove("hidden");
+
+  fetchServer(query, {}, (data, error) => {
+    if (!error) {
+      createUserList(data["getAllUsers"]);
+    } else {
+      if (error.extensions.code === "USER_NO_ACCESS") {
+        document.getElementById("cg-ds-user-manage").classList.add("hidden");
+        document.getElementById("cg-ds-no-access").classList.remove("hidden");
+      }
+
+      console.error("ERROR: " + error.message);
+    }
+
+    document.getElementById("cg-ds-man-refresh").classList.remove("loading");
+  });
+};
+
+const createUserList = (userList) => {
+  document.getElementById("cg-sd-man-userDisplay").innerHTML = "";
+
+  userList.forEach((user) => {
+    const name = user.name;
+    const role = user.role;
+    const verified = user.verified;
+    const email = user.email;
+    const id = user.id;
+
+    const displayRole = role === 0 ? "Member" : role === 1 ? "Administrator" : role === 2 ? "Master" : "Unknown";
+    const displayVerified = verified ? "verified" : "not verified";
+
+    const userDisplay = document.createElement("div");
+    userDisplay.innerHTML = `
+    <div class="cg-ds-man-user-display">
+      <div class="user-information">
+        <div class="user-name">${name}</div>
+        <div class="user-role">${displayRole}, ${displayVerified}</div>
+        <div class="user-email">${email}</div>
+      </div>
+
+      <div class="user-action">
+        <button class="cg-link-btn light" style="margin-right: 5px">View</button>
+        <button class="cg-link-btn light" ${verified ? "disabled" : ""}>${verified ? "Verified" : "Verify"}</button>
+      </div>
+    </div>`;
+
+    document.getElementById("cg-sd-man-userDisplay").appendChild(userDisplay);
+  });
+};
+
+const toggleSection = (section, callback) => {
+  const allDisplay = document.querySelectorAll(`[data-section]`);
+  const display = document.querySelector(`[data-section="${section}"]`);
+
+  const allButtons = document.querySelectorAll(`[data-for-section]`);
+  const button = document.querySelector(`[data-for-section="${section}"]`);
+
+  allDisplay.forEach((display) => {
+    display.classList.add("hidden");
+  });
+
+  allButtons.forEach((display) => {
+    display.classList.remove("selected");
+  });
+
+  display.classList.remove("hidden");
+  button.classList.add("selected");
+
+  callback ? callback() : null;
+};
